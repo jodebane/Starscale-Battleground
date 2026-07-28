3c4ee16831539c9cac8ad25c0e11eeae6a549c57
Joseph <josephfoley@rcn.com>
2026-07-27 20:21:49 -0400

    updated sql file

--- data/SSBG_card_database.sql
+++ data/SSBG_card_database.sql
@@ -1,13 +1,18 @@
+DROP database SSBG_cards;
 CREATE database SSBG_cards;
 
-CREATE TABLE cardslist(
-id int PRIMARY KEY,
+CREATE TABLE SSBG_cards.cardslist(
+card_id int PRIMARY KEY NOT NULL,
 card_name varchar(255) NOT NULL,
 card_type varchar(255) NOT NULL,
 card_subtype varchar(255),
 card_other_subtype varchar(255),
-card
-
-
+card_tags varchar(300),
+card_ep_cost int,
+card_mission_cost int,
+card_attack_power int,
+card_hit_points int,
+card_rules_text varchar(600) NOT NULL
 
 );
+
