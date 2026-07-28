e86467a74f0d9d6ff6ab01f98afd0d34e549fe8b
Joseph <josephfoley@rcn.com>
2026-07-27 20:10:30 -0400

    adds sql file and changes card info file to include a second subtype column

---
+++ data/SSBG_card_database.sql
@@ -0,0 +1,13 @@
+CREATE database SSBG_cards;
+
+CREATE TABLE cardslist(
+id int PRIMARY KEY,
+card_name varchar(255) NOT NULL,
+card_type varchar(255) NOT NULL,
+card_subtype varchar(255),
+card_other_subtype varchar(255),
+card
+
+
+
+);
